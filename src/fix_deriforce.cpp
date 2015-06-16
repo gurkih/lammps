8149bc82413a65b0a3daa070925d8a324a252e79
gurkih <markus.fritscher@fau.de>
2015-06-16 19:34:51 +0200

    cleaning up

--- src/fix_deriforce.cpp
+++ src/fix_deriforce.cpp
@@ -44,25 +44,20 @@ int ForceDerivative::setmask() {
 
 void ForceDerivative::sort(int myindex) {
 	//this doesnt work as sort is unable to access indicesofblah
-	printf("sorting ... \n");
-	double blah = indicesofclosestatoms[0][0];
+//	printf("sorting ... \n");
 //	double blab = distancesofclosestatoms[0][0];
-	printf("i did blah, myindex is %d %f \n", myindex, blah);
+	printf("i did blah, myindex is %d \n", myindex);
 	if (distancesofclosestatoms [myindex][2] < distancesofclosestatoms[myindex][0]) {
 
-		printf("1 \n");
 		int itmp = indicesofclosestatoms[myindex][2];
 		double dtmp = distancesofclosestatoms[myindex][2];
 
-		printf("2 \n");
 		indicesofclosestatoms[myindex][2] = indicesofclosestatoms[myindex][1];
 		distancesofclosestatoms[myindex][2]= distancesofclosestatoms[myindex][1];
 		
-		printf("3 \n");
 		indicesofclosestatoms[myindex][1] = indicesofclosestatoms[myindex][0];
 		distancesofclosestatoms[myindex][1]=  distancesofclosestatoms[myindex][0];
 
-		printf("4 \n");
 		indicesofclosestatoms[myindex][0] = itmp;
 		distancesofclosestatoms[myindex][0]= dtmp;
 	}
@@ -79,13 +74,14 @@ void ForceDerivative::sort(int myindex) {
 		distancesofclosestatoms[myindex][1]= dtmp;
 
 	}
-	printf("finished sorting! \n");
+	return;
+//	printf("finished sorting! \n");
 }
 
 double ForceDerivative::euclideandistance(double* firstatom, double* secondatom) {
-				printf("measuring ... \n");
+//				printf("measuring ... \n");
 				if (firstatom[0] == secondatom[0] && firstatom[1] == secondatom[1] && firstatom[2] == secondatom[2]) {
-					printf("done measuring! \n");
+//					printf("done measuring! \n");
 						return DBL_MAX; //return inf if we got handed over the same atom twice
 				}
 				//printf("%f", tmp[0]);
@@ -98,7 +94,7 @@ double ForceDerivative::euclideandistance(double* firstatom, double* secondatom)
 				double ydistance = pow(firstatom[1]-secondatom[1],2);
 				double zdistance = pow(firstatom[2]-secondatom[2],2);
 				double mydistance = sqrt(xdistance+ydistance+zdistance);
-				printf("done measuring! \n");
+//				printf("done measuring! \n");
 				return mydistance;
 }
 
@@ -108,15 +104,7 @@ double **indicesofclosestatoms;
 double **distancesofclostesatoms;
 
 void ForceDerivative::end_of_step() {
-
-
 	int nlocal = atom->nlocal;
-	//indicesofclosestatoms = new double[9999][3]; // just looking for the three closest atoms right now. this might change.
-	//distancesofclosestatoms = new double[9999][3];
-	
-
-//	extern double** indicesofclosestatoms;
-//	extern double** distancesofclostesatoms;
 	indicesofclosestatoms = new double*[3];
 	distancesofclosestatoms = new double*[3];
 	double foo[3] = {0,0,0};
@@ -124,90 +112,66 @@ void ForceDerivative::end_of_step() {
 		indicesofclosestatoms[i] = foo;
 		distancesofclosestatoms[i] = foo;
 	}
-//	indicesofclosestatoms[0][0] = 42;
-	printf("i just did stuff\n");
-
 	double deriforceoutput[nlocal][3];
 	double forceoutput[nlocal][3];
 	double speedoutput[nlocal][3];
-
 	float averagedenominator[3] = {0,0,0};
-
 	float averagederif[3] = {0,0,0};
-//	printf("i am being run!");
-
-
-//	printf("; \n");
-	for (int indexOfParticle = 0; indexOfParticle < nlocal; ++indexOfParticle) {
-	
 	double **poscopy = atom->x;
+/*
 	for(int i = 0; i < 10; i++) {
 		for (int j = 0; j < 3; j++) {
 			printf("poscopy %d/%d = %f ",i, j, poscopy[i][j]);
 		}
 		printf("\n");
 	}
+*/
 	double **forcecopy = atom->f;
 	double **speedcopy = atom->v;
 	int** specialcopy = atom->bond_type;
 	bool ifoundsomething = false;
-//	printf("i managed to copy!\n");
+	for (int indexOfParticle = 0; indexOfParticle < nlocal; ++indexOfParticle) {
 //	printf("my bond type is: %d \n",atom->bond_type[2][2]);
-
-	indicesofclosestatoms[indexOfParticle][0];
 		if (atom->mask[indexOfParticle] & groupbit) {
-		//	printf("Hello World");
-			
-			for (int i = 0; i < 3; i++) {
-				indicesofclosestatoms [indexOfParticle][i] = i;
-				if (indexOfParticle <=2) {
-//					indicesofclosestatoms [indexOfParticle][i]+=3;
-				}	
-				printf("i managed to init! \n");
-
+		for (int i = 0; i < 3; i++) {
+			indicesofclosestatoms [indexOfParticle][i] = i;
+			if (indexOfParticle <=2) {
+			}	
 // we have to create local copys to avoid some scope trouble.
-
-				double first [3];
-				double second [3];
-				for (int x = 0; x < 3; x++) {
-					first[x] = poscopy[indexOfParticle][x];
-					int foo = indicesofclosestatoms[indexOfParticle][i];
-					second[x] = poscopy[foo][x];
-				}
-				//distancesofclosestatoms[indexOfParticle][i] = euclideandistance(indexOfParticle,indicesofclosestatoms [indexOfParticle][i]);i
-				distancesofclosestatoms[indexOfParticle][i] = euclideandistance(first, second);
-				
+			double first [3];
+			double second [3];
+			for (int x = 0; x < 3; x++) {
+				first[x] = poscopy[indexOfParticle][x];
+				int foo = indicesofclosestatoms[indexOfParticle][i];
+				second[x] = poscopy[foo][x];
 			}
-			printf("my distance is %f \n", distancesofclosestatoms[indexOfParticle][0]);
-
-			sort(indexOfParticle);
-
-			for (int i = 0; i < nlocal; i++) {
-				ifoundsomething = false;
-				if (i == indexOfParticle) {
-					break;
-				}
-
+			//distancesofclosestatoms[indexOfParticle][i] = euclideandistance(indexOfParticle,indicesofclosestatoms [indexOfParticle][i]);i
+			distancesofclosestatoms[indexOfParticle][i] = euclideandistance(first, second);
+		}
+		printf("my distance is %f \n", distancesofclosestatoms[indexOfParticle][0]);
+		sort(indexOfParticle);
+		for (int i = 0; i < nlocal; i++) {
+			ifoundsomething = false;
+			if (i == indexOfParticle) {
+				break;
+			}
 // we have to create local copys to avoid some scope trouble.
-
-				double first [3];
-				double second [3];
-				for (int x = 0; x < 3; x++) {
-					first[x] = poscopy[indexOfParticle][x];
-					second[x] = poscopy[i][x];
-				}
-
-				double mydistance = euclideandistance(first, second);
-
-				if (mydistance < distancesofclosestatoms[indexOfParticle][2]) {
-					indicesofclosestatoms[indexOfParticle][2] = i;
-					distancesofclosestatoms[indexOfParticle][2] = mydistance;
-					ifoundsomething = true;
-				}
-				if (ifoundsomething) {				
-					sort(indexOfParticle);
-				}
+			double first [3];
+			double second [3];
+			for (int x = 0; x < 3; x++) {
+				first[x] = poscopy[indexOfParticle][x];
+				second[x] = poscopy[i][x];
 			}
+			double mydistance = euclideandistance(first, second);
+			if (mydistance < distancesofclosestatoms[indexOfParticle][2]) {
+				indicesofclosestatoms[indexOfParticle][2] = i;
+				distancesofclosestatoms[indexOfParticle][2] = mydistance;
+				ifoundsomething = true;
+			}
+			if (ifoundsomething) {				
+				sort(indexOfParticle);
+			}
+		}
 //			printf("%d, %d, %d; \n",poscopy[indexOfParticle][0],poscopy[indexOfParticle][1],poscopy[indexOfParticle][2]);
 		}
 	}
